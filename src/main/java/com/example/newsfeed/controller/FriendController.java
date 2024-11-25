3094188c41871b45345f97ed661762192f07e813
dirtbagss <dirtbagag4@gmail.com>
2024-11-25 09:18:50 +0900

    feat : userId session값으로 입력

--- src/main/java/com/example/newsfeed/controller/FriendController.java
+++ src/main/java/com/example/newsfeed/controller/FriendController.java
@@ -3,6 +3,8 @@ package com.example.newsfeed.controller;
 import com.example.newsfeed.dto.FriendRequestDto;
 import com.example.newsfeed.dto.FriendResponseDto;
 import com.example.newsfeed.service.FriendService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +22,17 @@ public class FriendController {
     }
 
     @GetMapping
-    public ResponseEntity<List<FriendResponseDto>> getFriendList(@PathVariable Long userId) {
+    public ResponseEntity<List<FriendResponseDto>> getFriendList(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        Long userId = (Long) session.getAttribute("SESSION_KEY");
         List<FriendResponseDto> friends = friendService.getFriends(userId);
         return new ResponseEntity<>(friends, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Void> addFriendRequest(@PathVariable Long userId, @RequestBody FriendRequestDto friendRequestDTO) {
+    public ResponseEntity<Void> addFriendRequest(HttpServletRequest request, @RequestBody FriendRequestDto friendRequestDTO) {
+        HttpSession session = request.getSession(false);
+        Long userId = (Long) session.getAttribute("SESSION_KEY");
         try {
             friendService.addFriendRequest(userId, friendRequestDTO.getTargetId());
             return new ResponseEntity<>(HttpStatus.OK);
@@ -36,7 +42,9 @@ public class FriendController {
     }
 
     @DeleteMapping("/{targetId}")
-    public ResponseEntity<Void> deleteFriend(@PathVariable Long userId, @PathVariable Long targetId) {
+    public ResponseEntity<Void> deleteFriend(HttpServletRequest request, @PathVariable Long targetId) {
+        HttpSession session = request.getSession(false);
+        Long userId = (Long) session.getAttribute("SESSION_KEY");
         try {
             friendService.deleteFriend(userId, targetId);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -46,7 +54,9 @@ public class FriendController {
     }
 
     @GetMapping("/{targetId}")
-    public ResponseEntity<FriendResponseDto> findFriend(@PathVariable Long userId, @PathVariable Long targetId) {
+    public ResponseEntity<FriendResponseDto> findFriend(HttpServletRequest request, @PathVariable Long targetId) {
+        HttpSession session = request.getSession(false);
+        Long userId = (Long) session.getAttribute("SESSION_KEY");
         try {
             FriendResponseDto friend = friendService.findFriend(userId, targetId);
             return new ResponseEntity<>(friend, HttpStatus.OK);
@@ -56,7 +66,9 @@ public class FriendController {
     }
 
     @PatchMapping("/{targetId}")
-    public ResponseEntity<Void> acceptFriendRequest(@PathVariable Long userId, @PathVariable Long targetId) {
+    public ResponseEntity<Void> acceptFriendRequest(HttpServletRequest request, @PathVariable Long targetId) {
+        HttpSession session = request.getSession(false);
+        Long userId = (Long) session.getAttribute("SESSION_KEY");
         try {
             friendService.acceptFriendRequest(userId, targetId);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -66,7 +78,9 @@ public class FriendController {
     }
 
     @DeleteMapping("/{targetId}/reject")
-    public ResponseEntity<Void> rejectFriendRequest(@PathVariable Long userId, @PathVariable Long targetId) {
+    public ResponseEntity<Void> rejectFriendRequest(HttpServletRequest request, @PathVariable Long targetId) {
+        HttpSession session = request.getSession(false);
+        Long userId = (Long) session.getAttribute("SESSION_KEY");
         try {
             friendService.rejectFriendRequest(userId, targetId);
             return new ResponseEntity<>(HttpStatus.OK);
